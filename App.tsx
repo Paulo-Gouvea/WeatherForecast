3cd10122f126864554ed1604eaee88939f2fe470
Paulo-Gouvea <paulogouvea1994@gmail.com>
2022-02-03 16:01:46 -0300

    style: added custom colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-import { Home } from "./screens/Home";
+import AppLoading from "expo-app-loading";
+import { 
+  useFonts,
+  RobotoSlab_300Light,
+  RobotoSlab_500Medium
+} from '@expo-google-fonts/roboto-slab';
+
+import { ThemeProvider } from "styled-components";
+import theme from "./src/styles/theme";
+
+import { Home } from "./src/screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_300Light,
+    RobotoSlab_500Medium
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+    colors: {
+        custom_white: '#FFFFFF',
+        custom_color: '#828CAE',
+        custom_gray: '#CCCFDA',
+        custom_navbar_icons: '#557EAE',
+        custom_body: '#101A39',
+        custom_main_background: '#060D26',
+        custom_up_body: '#2C303F',
+        custom_weather_card: '#272F3A'
+    },
+
+    fonts: {
+        light: 'RobotoSlab_300Light',
+        medium: 'RobotoSlab_500Medium'
+    }
+}
